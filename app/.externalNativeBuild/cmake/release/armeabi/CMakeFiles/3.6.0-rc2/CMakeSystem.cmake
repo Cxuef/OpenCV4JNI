5e50c64f7ed633b6a898aea12d969c94ec41e444
xuefan.chen <xuefan.chen@ck-telecom.com>
2016-11-28 18:00:10 +0800

    Add OpenCV native tips by use cmake/ndkBuild for exist ndk project
    modified:   app/build.gradle
    new file:   app/src/main/cpp/CMakeLists.txt

---
+++ app/.externalNativeBuild/cmake/release/armeabi/CMakeFiles/3.6.0-rc2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Linux-3.13.0-53-generic")
+set(CMAKE_HOST_SYSTEM_NAME "Linux")
+set(CMAKE_HOST_SYSTEM_VERSION "3.13.0-53-generic")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "x86_64")
+
+include("/home/eirot/MyDev/android-sdk-linux/cmake/3.6.3155560/android.toolchain.cmake")
+
+set(CMAKE_SYSTEM "Android-21")
+set(CMAKE_SYSTEM_NAME "Android")
+set(CMAKE_SYSTEM_VERSION "21")
+set(CMAKE_SYSTEM_PROCESSOR "armv5te")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
---
+++ app/.externalNativeBuild/ndkBuild/debug/obj/local/armeabi-v7a/objs/grayprocess/GrayProcess.o.d
@@ -0,0 +1,188 @@
+/home/eirot/MyDev/study/AndroidApps/OpenCV4JNI/app/.externalNativeBuild/ndkBuild/debug/obj/local/armeabi-v7a/objs/grayprocess/GrayProcess.o: \
+ /home/eirot/MyDev/study/AndroidApps/OpenCV4JNI/app/src/main/jni/GrayProcess.cpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/photo.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/tracking_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/deque \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/calib3d.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/affine.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h \
+ /home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/ml.hpp \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/map \
+ /home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream \
+ /home/eirot/MyDev/study/AndroidApps/OpenCV4JNI/app/src/main/jni/com_ckt_eirot_opencv4jni_GrayProcess.h
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/opencv.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal/defs.h:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdint:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/hal.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/sse_utils.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/imgproc_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgproc/types_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/photo.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/photo/photo_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/tracking.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/background_segm.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/video/tracking_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/features2d.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/miniflann.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/defines.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/flann/config.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/calib3d.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/core/affine.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/calib3d/calib3d_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui.hpp:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/highgui/highgui_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/imgcodecs/imgcodecs_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/videoio/videoio_c.h:
+
+/home/eirot/MyDev/Opencv/OpenCV-android-sdk/sdk/native/jni/include/opencv2/ml.hpp:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/map:
+
+/home/eirot/MyDev/android-sdk-linux/ndk/android-ndk-r12b/sources/cxx-stl/gnu-libstdc++/4.9/include/iostream:
+
+/home/eirot/MyDev/study/AndroidApps/OpenCV4JNI/app/src/main/jni/com_ckt_eirot_opencv4jni_GrayProcess.h:
