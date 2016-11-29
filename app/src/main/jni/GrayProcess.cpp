68ecac67bbba20ce32c2ab012c1be2e46036357c
xuefan.chen <xuefan.chen@ck-telecom.com>
2016-11-29 17:19:41 +0800

    Add Jni log and replace ic_lancher icon

--- app/src/main/jni/GrayProcess.cpp
+++ app/src/main/jni/GrayProcess.cpp
@@ -1,10 +1,18 @@
 #include <jni.h>
+#include <android/log.h>
 #include <opencv2/opencv.hpp>
+
+#include <stdio.h>
 #include "com_ckt_eirot_opencv4jni_GrayProcess.h"
 
 extern "C" {
 using namespace cv;
 using namespace std;
+
+#define LOG_TAG  "GraProcessJni"
+#define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
+#define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
+#define LOGD(...) ((void)__android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__))
 
 
 JNIEXPORT jintArray JNICALL Java_com_ckt_eirot_opencv4jni_GrayProcess_grayProcess(JNIEnv *env,
